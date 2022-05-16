a17c116700b7f45700313541e89cfecc0b2b51e0
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-16 10:39:23 +0600

    added booking information fron client end to database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const query = require('express/lib/middleware/query');
 
 
 require('dotenv').config();
-const port = process .env. PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express()
 
@@ -17,21 +18,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4nilh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect()
-        const serviceCollection = client.db('Doctors_portal').collection('servies')
+        const serviceCollection = client.db('Doctors_portal').collection('servies');
+        const bookingCollection = client.db('Doctors_portal').collection('bookings');
 
-        app.get('/service', async(req,res)=>{
+        app.get('/service', async (req, res) => {
             const query = {};
-            const cursor= serviceCollection.find(query);
+            const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query)
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result })
+        })
     }
 
-   
-    finally{
+
+    finally {
 
     }
 }
@@ -39,10 +53,10 @@ async function run(){
 run().catch(console.dir);
 
 
-app.get('/' , (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Doctors Port ${port}`)
 })
